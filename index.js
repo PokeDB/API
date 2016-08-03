1aa6fef8e34efd16ade6d976edf2a4ed0758a591
Derek Ho <derek.ho@gmail.com>
2016-08-02 22:09:44 -0400

    Support PokemonGo-Map webhook

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 
 // routes
 var pokemon = require('./pokemon');
+
+// Parse incoming application/json
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
   res.json({
@@ -16,4 +21,3 @@ app.use('/pokemon', pokemon);
 app.listen(80, function () {
   console.log('app listening on port 80!');
 });
-
