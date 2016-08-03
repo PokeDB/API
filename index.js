8b73a85bcccd7d0153973d31f15ce64c04e43ed7
root <ps0296@gmail.com>
2016-08-03 04:36:43 +0000

    set server port based on env

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ app.get('/', function(req, res) {
 
 app.use('/pokemon', pokemon);
 
-app.listen(80, function () {
-  console.log('app listening on port 80!');
+var port = process.env.NODE_ENV === 'production' ? 80 : 8080;
+
+app.listen(port, function () {
+  console.log(process.env.NODE_ENV + ' app listening on port '+port+'!');
 });
+
