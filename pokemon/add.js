61ed6ebc346ea45e409e928ff4e0b69e749d0c9f
Pardeep <ps0296@gmail.com>
2016-08-03 01:43:22 -0400

    create data object ready to be inserted into mongo, format geo properly so mongo can index it

--- pokemon/add.js
+++ pokemon/add.js
@@ -4,41 +4,52 @@ var _ = require('lodash');
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res) {
-  res.json({success: false});
-});
-
 var POKEMONGO_MAP_FIELDS = [
-    'encounter_id', 'spawnpoint_id', 'pokemon_id', 'latitude',
-    'longitude', 'disappear_time', 'time_until_hidden_ms',
-    'last_modified_time'
-  ];
+  'spawnpoint_id',
+  'pokemon_id',
+  'latitude',
+  'longitude',
+  'disappear_time',
+];
 
 router.post('/', function(req, res) {
   let rawData = req.body;
 
   if (!rawData) {
     res.status(400).json({ error: 'Json body is required' });
-
     return;
   }
 
   // PokemonGo-Map webhook format
   if ('type' in rawData && 'message' in rawData) {
-    let
-      parsedData = _.pick(
-        rawData.message,
-        POKEMONGO_MAP_FIELDS
-      );
+    let parsedData = _.pick(
+      rawData.message,
+      POKEMONGO_MAP_FIELDS
+    );
+    if (_.size(parsedData) !== POKEMONGO_MAP_FIELDS.length) {
+      res.status(400).json({error: 'Unknown data format.'});
+      return;
+    }
+
+    let data = {
+      _id: parsedData.pokemon_id + ':' + parsedData.disappear_time,
+      spawnpoint_id: parsedData.spawnpoint_id,
+      pokemon_id: parsedData.pokemon_id,
+      geo: {
+        type: 'Point',
+        coordinates: [parsedData.longitude, parsedData.latitude]
+      },
+      disappear_time: parsedData.disappear_time
+    }
 
     // Add parsedData to db here
-    console.log(parsedData);
+    console.log(data);
 
     res.json({
       success: true
     });
   } else {
-    res.status(400).json({ error: 'Unknown data format' });
+    res.status(400).json({error: 'Unknown data format.'});
   }
 });
 
