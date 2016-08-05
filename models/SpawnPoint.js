ef8670e78d7aa22ba0554e207f0e339d715130b9
ps0296 <ps0296@gmail.com>
2016-08-05 16:33:23 -0400

    spawnPoints: add index on disappearTime which expires after 5 days

--- models/SpawnPoint.js
+++ models/SpawnPoint.js
@@ -10,8 +10,8 @@ var spawnPointSchema = new Schema({
 }, {collection: 'spawnPoints', typeKey: '$type'});
 
 spawnPointSchema.index({loc: '2dsphere', pokemonId: 1});
+spawnPointSchema.index({disappearTime: 1}, {expireAfterSeconds: 432000});
 
 var SpawnPoint = mongoose.model('spawnPoints', spawnPointSchema);
 
 module.exports = SpawnPoint;
-
